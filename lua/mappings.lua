5c2b676a42b9defcd2de9f75f491babdd19c0b8c
Michał Duszyk <michal@duszyk.com>
2023-11-19 18:21:52 +0100

    switch window mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,5 @@
 local mappings = {
+    ["<tab>"] = { "<c-w>w", "Switch window" },
     ["<leader>n"] = { "<cmd>Neotree toggle<cr>", "Toggle neo tree" },
     ["<leader>b"] = { "<cmd>Neotree buffers toggle<cr>", "Toggle neo tree buffers" },
     ["<leader>g"] = { "<cmd>Neotree git_status toggle<cr>", "Toggle neo tree git status" },
