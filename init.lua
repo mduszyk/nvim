fa3ddbefb5a8257727d2b4227c7114bb21a0d5a4
Michał Duszyk <michal@duszyk.com>
2023-11-19 17:54:59 +0100

    adding mappings

--- init.lua
+++ init.lua
@@ -4,6 +4,10 @@ local lazy = require("lazy")
 lazy.setup(plugins)
 
 vim.g.mapleader = " "
+local mappings = require("mappings")
+local wk = require("which-key") 
+wk.register(mappings)
+
 vim.opt.number = true
 --vim.opt.colorcolumn = "80"
 
