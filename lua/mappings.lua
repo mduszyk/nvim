b77d571fb9ac57a9c15cb367ff42e226a1e26e40
Michał Duszyk <michal@duszyk.com>
2023-11-19 18:39:56 +0100

    map esc to noh

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,10 @@
 local mappings = {
+    ["<Esc>"] = { "<cmd> noh<cr>", "Clear highlights" },
     ["<tab>"] = { "<c-w>w", "Switch window" },
     ["<leader>n"] = { "<cmd>Neotree toggle<cr>", "Toggle neo tree" },
     ["<leader>b"] = { "<cmd>Neotree buffers toggle<cr>", "Toggle neo tree buffers" },
     ["<leader>g"] = { "<cmd>Neotree git_status toggle<cr>", "Toggle neo tree git status" },
+    -- TODO Neotree document_symbols (requires lsp)
     ["<leader>f"] = { name = "Telescope" },
     ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Telescope find files" },
     ["<leader>fw"] = { "<cmd>Telescope live_grep<cr>", "Telescope grep" },
