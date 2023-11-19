fe2f43fb2d8396222d9ebf156980c71afb6d15c4
Michał Duszyk <michal@duszyk.com>
2023-11-19 18:59:28 +0100

    indentation config and save mapping

--- init.lua
+++ init.lua
@@ -6,8 +6,18 @@ lazy.setup(plugins)
 vim.g.mapleader = " "
 local mappings = require("mappings")
 local wk = require("which-key") 
-wk.register(mappings, {mode = "n"})
+wk.register(mappings["n"], {mode = "n"})
+wk.register(mappings["i"], {mode = "i"})
+wk.register(mappings["v"], {mode = "v"})
 
 vim.opt.number = true
---vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
+vim.cmd [[highlight ColorColumn ctermbg=DarkGray guibg=DarkGray]]
+
+-- default indentation
+vim.opt.ts = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- use spaces instead of tabs
+vim.opt.expandtab = true
 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,15 +1,25 @@
 local mappings = {
-    ["<Esc>"] = { "<cmd> noh<cr>", "Clear highlights" },
-    ["<tab>"] = { "<c-w>w", "Switch window" },
-    ["<leader>n"] = { "<cmd>Neotree toggle<cr>", "Toggle neo tree" },
-    ["<leader>b"] = { "<cmd>Neotree buffers toggle<cr>", "Toggle neo tree buffers" },
-    ["<leader>g"] = { "<cmd>Neotree git_status toggle<cr>", "Toggle neo tree git status" },
-    -- TODO Neotree document_symbols (requires lsp)
-    ["<leader>f"] = { name = "Telescope" },
-    ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Telescope find files" },
-    ["<leader>fw"] = { "<cmd>Telescope live_grep<cr>", "Telescope grep" },
-    ["<leader>fb"] = { "<cmd>Telescope buffers<cr>", "Telescope buffers" },
-    ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Telescope help tags" },
+    n = {
+        ["<c-s>"] = { "<cmd>update<cr>", "Save" },
+        ["<esc>"] = { "<cmd>noh<cr>", "Clear highlights" },
+        ["<tab>"] = { "<c-w>w", "Switch window" },
+        ["<leader>n"] = { "<cmd>Neotree toggle<cr>", "Toggle neo tree" },
+        ["<leader>b"] = { "<cmd>Neotree buffers toggle<cr>", "Toggle neo tree buffers" },
+        ["<leader>g"] = { "<cmd>Neotree git_status toggle<cr>", "Toggle neo tree git status" },
+        -- TODO Neotree document_symbols (requires lsp)
+        ["<leader>f"] = { name = "Telescope" },
+        ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Telescope find files" },
+        ["<leader>fw"] = { "<cmd>Telescope live_grep<cr>", "Telescope grep" },
+        ["<leader>fb"] = { "<cmd>Telescope buffers<cr>", "Telescope buffers" },
+        ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Telescope help tags" },
+    },
+    i = {
+        ["<c-s>"] = { "<esc><cmd>update<cr>", "Save" },
+
+    },
+    v = {
+        ["<c-s>"] = { "<esc><cmd>update<cr>", "Save" },
+    }
 }
 
 return mappings
