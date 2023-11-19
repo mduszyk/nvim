a45080bf84fefae4cbb57937c86ffc6d790a43a7
Michał Duszyk <michal@duszyk.com>
2023-11-19 17:55:08 +0100

    adding mappings

---
+++ lua/mappings.lua
@@ -0,0 +1,6 @@
+local mappings = {
+  ["<leader>n"] = { "<cmd>Neotree toggle<cr>", "Toggle neo tree" },
+  ["<leader>b"] = { "<cmd>Neotree buffers toggle<cr>", "Toggle neo tree buffers" },
+}
+
+return mappings
