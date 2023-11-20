09c7d8d4d0e0a1b7cf1414eb049fee2ef0e1bf0c
Michał Duszyk <michal@duszyk.com>
2023-11-20 16:51:45 +0100

    refactoring mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,42 +1,42 @@
-local mappings = {
-    n = {
-        ["<esc>"] = { "<cmd>noh<cr>", "Clear highlights" },
-        ["<c-s>"] = { "<cmd>update<cr>", "Save" },
-        ["<tab>"] = { "<c-w>w", "Switch window" },
-        ["\\"] = { "<cmd>Neotree reveal<cr>", "Neotree show current file" },
-        ["<leader>a"] = { "ggVG", "Select all" },
-        ["<leader>q"] = { "<cmd>q<cr>", "Quit" },
-        ["<leader>n"] = { "<cmd>Neotree toggle<cr>", "Toggle Neotree" },
-        ["<leader>b"] = { "<cmd>Neotree buffers toggle<cr>", "Toggle Neotree buffers" },
-        ["<leader>g"] = { "<cmd>Neotree git_status toggle<cr>", "Toggle Neotree git status" },
-        -- TODO Neotree document_symbols (requires lsp)
-        ["<leader>f"] = { name = "Telescope" },
-        ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Telescope find files" },
-        ["<leader>fw"] = { "<cmd>Telescope live_grep<cr>", "Telescope grep" },
-        ["<leader>fb"] = { "<cmd>Telescope buffers<cr>", "Telescope buffers" },
-        ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Telescope help tags" },
-        ["<leader>u"] = { name = "UI" },
-        ["<leader>un"] = { "<cmd>setlocal number!<cr>", "Toggle line numbering" },
-        ["<leader>t"] = { "<cmd>ToggleTerm<cr>", "Toggle terminal" },
-    },
-    i = {
-        ["<c-s>"] = { "<esc><cmd>update<cr>", "Save" },
+local mappings = {}
 
-    },
-    v = {
-        ["<c-s>"] = { "<esc><cmd>update<cr>", "Save" },
-    },
+mappings.n = {
+    ["<esc>"] = { "<cmd>noh<cr>", "Clear highlights" },
+    ["<c-s>"] = { "<cmd>update<cr>", "Save" },
+    ["<tab>"] = { "<c-w>w", "Switch window" },
+    ["\\"] = { "<cmd>Neotree reveal<cr>", "Neotree show current file" },
+    ["<leader>a"] = { "ggVG", "Select all" },
+    ["<leader>q"] = { "<cmd>q<cr>", "Quit" },
+    ["<leader>n"] = { "<cmd>Neotree toggle<cr>", "Toggle Neotree" },
+    ["<leader>b"] = { "<cmd>Neotree buffers toggle<cr>", "Toggle Neotree buffers" },
+    ["<leader>g"] = { "<cmd>Neotree git_status toggle<cr>", "Toggle Neotree git status" },
+    -- TODO Neotree document_symbols (requires lsp)
+    ["<leader>f"] = { name = "Telescope" },
+    ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Telescope find files" },
+    ["<leader>fw"] = { "<cmd>Telescope live_grep<cr>", "Telescope grep" },
+    ["<leader>fb"] = { "<cmd>Telescope buffers<cr>", "Telescope buffers" },
+    ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Telescope help tags" },
+    ["<leader>u"] = { name = "UI" },
+    ["<leader>un"] = { "<cmd>setlocal number!<cr>", "Toggle line numbering" },
+    ["<leader>t"] = { "<cmd>ToggleTerm<cr>", "Toggle terminal" },
+}
+
+mappings.i = {
+    ["<c-s>"] = { "<esc><cmd>update<cr>", "Save" },
+}
+
+mappings.v = {
+    ["<c-s>"] = { "<esc><cmd>update<cr>", "Save" },
 }
 
 function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  -- enter normal mode
-  vim.keymap.set('t', '<c-x>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    local opts = {buffer = 0}
+    vim.keymap.set('t', '<c-x>', [[<C-\><C-n>]], { buffer = 0, desc = "Enter normal mode" })
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
 function mappings.register()
