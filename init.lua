7dbccfae33ec7fd929aa2f8075f9fc3882f905c3
Michał Duszyk <michal@duszyk.com>
2023-11-20 10:18:27 +0100

    cleaning init

--- init.lua
+++ init.lua
@@ -8,12 +8,9 @@ vim.g.mapleader = " "
 vim.opt.ts = 4
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-
--- use spaces instead of tabs
 vim.opt.expandtab = true
-vim.opt.number = true
 
--- margin
+vim.opt.number = true
 vim.opt.colorcolumn = "100"
 vim.cmd([[highlight ColorColumn ctermbg=DarkGray guibg=DarkGray]])
 
