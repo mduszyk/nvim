19a8501bcd836876a82d815302f37f1a3072d72f
Michał Duszyk <michal@duszyk.com>
2023-11-21 15:15:07 +0100

    clipboard

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 require("plugins")
 require("mappings")
 
+vim.opt.number = true
+vim.opt.clipboard = "unnamedplus"
 -- default indentation
 vim.opt.ts = 4
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.number = true
 
 -- color 81st column
 vim.cmd([[autocmd! BufRead * lua vim.fn.matchadd('CursorColumn', '\\%81v', 100)]])
