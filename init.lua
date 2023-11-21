f3366299f00e88d09aff8d63a16924675f267fd7
Michał Duszyk <michal@duszyk.com>
2023-11-21 15:12:04 +0100

    color column

--- init.lua
+++ init.lua
@@ -6,8 +6,7 @@ vim.opt.ts = 4
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.number = true
--- vim.opt.colorcolumn = "100"
--- im.cmd([[highlight ColorColumn ctermbg=DarkGray guibg=DarkGray]])
 
+-- color 81st column
+vim.cmd([[autocmd! BufRead * lua vim.fn.matchadd('CursorColumn', '\\%81v', 100)]])
