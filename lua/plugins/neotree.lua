068cb2d5ec663d0f8654d91d778940af0ce40668
Michał Duszyk <michal@duszyk.com>
2023-11-22 14:51:25 +0100

    fixing neotree mappings

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -17,29 +17,88 @@ return {
                     with_markers = false
                 }
             },
+            use_default_mappings = false,
             window = {
                 width = 32,
                 mapping_options = { noremap = true, nowait = true, },
                 mappings = {
-                    ["o"] = {"open", nowait = true, noremap = true},
+                    ["<space>"] = {
+                        "toggle_node",
+                        nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+                    },
+                    ["<2-LeftMouse>"] = "open",
+                    ["o"] = "open",
+                    ["<esc>"] = "revert_preview",
+                    ["P"] = { "toggle_preview", config = { use_float = true } },
+                    ["S"] = "open_split",
+                    -- ["S"] = "split_with_window_picker",
+                    ["s"] = "open_vsplit",
+                    -- ["s"] = "vsplit_with_window_picker",
+                    ["t"] = "open_tabnew",
+                    -- ["<cr>"] = "open_drop",
+                    -- ["t"] = "open_tab_drop",
+                    ["w"] = "open_with_window_picker",
+                    ["C"] = "close_node",
+                    --["z"] = "close_all_nodes",
+                    --["Z"] = "expand_all_nodes",
+                    ["R"] = "refresh",
+                    ["a"] = {
+                        "add",
+                        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                        config = {
+                            show_path = "none" -- "none", "relative", "absolute"
+                        }
+                    },
+                    ["A"] = "add_directory", -- also accepts the config.show_path option.
+                    ["d"] = "delete",
+                    ["r"] = "rename",
+                    ["y"] = "copy_to_clipboard",
+                    ["x"] = "cut_to_clipboard",
+                    ["p"] = "paste_from_clipboard",
+                    ["c"] = "copy", -- takes text input for destination, also accepts the config.show_path option
+                    ["m"] = "move", -- takes text input for destination, also accepts the config.show_path option
+                    ["e"] = "toggle_auto_expand_width",
+                    ["q"] = "close_window",
+                    ["?"] = "show_help",
+                    ["<"] = "prev_source",
+                    [">"] = "next_source",
                 }
             },
             filesystem = {
                 use_libuv_file_watcher = true,
                 window = {
                     mappings = {
+                        ["H"] = "toggle_hidden",
+                        ["/"] = "fuzzy_finder",
+                        ["D"] = "fuzzy_finder_directory",
+                        ["f"] = "filter_on_submit",
+                        ["<C-x>"] = "clear_filter",
+                        ["<bs>"] = "navigate_up",
+                        ["."] = "set_root",
+                        ["[g"] = "prev_git_modified",
+                        ["]g"] = "next_git_modified",
                     }
                 }
             },
             buffers = {
                 window = {
                     mappings = {
+                        ["<bs>"] = "navigate_up",
+                        ["."] = "set_root",
+                        ["d"] = "buffer_delete",
                     }
                 }
             },
             git_status = {
                 window = {
                     mappings = {
+                        ["A"] = "git_add_all",
+                        ["gu"] = "git_unstage_file",
+                        ["ga"] = "git_add_file",
+                        ["gr"] = "git_revert_file",
+                        ["gc"] = "git_commit",
+                        ["gp"] = "git_push",
+                        ["gg"] = "git_commit_and_push",
                     }
                 }
             }
