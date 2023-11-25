e3babe768b93d7719dbc89a3ce3f325477392896
Michał Duszyk <michal@duszyk.com>
2023-11-25 12:19:44 +0100

    adding spell config

--- init.lua
+++ init.lua
@@ -12,6 +12,9 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.spelllang = "en_gb"
+vim.opt.spell = true
+
 -- color 101st column
 vim.api.nvim_create_autocmd({"BufRead"}, {
     pattern = {"*"},
